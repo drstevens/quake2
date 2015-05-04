97a836b71b1fb77c4b952d172e187a36f1435770
Jack Whitham <jack.d.whitham@gmail.com>
2015-05-04 22:36:17 +0100

    update test script

--- q.sh
+++ q.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -xe
 cd /home/jack/durden/quake2
-dchroot -- make -C $PWD
+dchroot -- make -C $PWD release
 cd bin
 mkdir -p baseq2
 cd baseq2
 ln -sf ~/durden/Quake-2/headless_quake2/baseq2/pak0.pak
 ln -sf ../game.so
 cd ..
-./quake2 +set vid_fullscreen 0
+./quake2 +set vid_fullscreen 0 +set sw_mode 5
 
