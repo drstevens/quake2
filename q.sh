286e1a207a1c4933a318c456bb232322bab261fd
Jack Whitham <jack.d.whitham@gmail.com>
2015-05-04 22:02:30 +0100

    ref_soft is back! but 8 bits are not enough for me

--- q.sh
+++ q.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
-cd /home/jack/durden/quake2/bin
-ln -sf ~/durden/Quake-2/headless_quake2/baseq2 
+#!/bin/bash -xe
+cd /home/jack/durden/quake2
+dchroot -- make -C $PWD
+cd bin
+mkdir -p baseq2
+cd baseq2
+ln -sf ~/durden/Quake-2/headless_quake2/baseq2/pak0.pak
+ln -sf ../game.so
+cd ..
 ./quake2 +set vid_fullscreen 0
 
